4804de91ee60ce8b18243cad16669b69835f84ac
Mike <mdebree@winbase.nl>
2018-03-16 21:28:58 +0100

    Add MvvmCross starter template

---
+++ SnackPlanning/SnackPlanning.Droid/obj/Debug/android/src/md52f336b0f220d4434b3bc5a9b855e8749/SplashScreen.java
@@ -0,0 +1,38 @@
+package md52f336b0f220d4434b3bc5a9b855e8749;
+
+
+public class SplashScreen
+	extends mvvmcross.droid.views.MvxSplashScreenActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("SnackPlanning.Droid.SplashScreen, SnackPlanning.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SplashScreen.class, __md_methods);
+	}
+
+
+	public SplashScreen ()
+	{
+		super ();
+		if (getClass () == SplashScreen.class)
+			mono.android.TypeManager.Activate ("SnackPlanning.Droid.SplashScreen, SnackPlanning.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
